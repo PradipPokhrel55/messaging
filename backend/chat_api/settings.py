164df15ab979e161ac32ce7dcecd8cc051d5325f
Pradip Pokhrel <136249839+PradipPokhrel55@users.noreply.github.com>
2025-07-09 13:49:49 +0545

    added gunicorn

--- backend/chat_api/settings.py
+++ backend/chat_api/settings.py
@@ -97,6 +97,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
